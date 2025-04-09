fc86f9bf272f77b9fc1790a80389ba556a888131
Chamith-Bhanuka <c.bhanukawp2002@gmail.com>
2025-04-09 23:12:10 +0530

    create typing function to roles

---
+++ app.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const roles = ["Web Developer", "Software Engineer", "Embedded System Engineer"];
+    const roleElement = document.getElementById("role");
+    let roleIndex = 0;
+    let letterIndex = 0;
+    let typingInterval;
+
+    function typeRole() {
+        roleElement.style.opacity = 0;
+        setTimeout(() => {
+            roleElement.textContent = "";
+            letterIndex = 0;
+            typingInterval = setInterval(() => {
+                if (letterIndex < roles[roleIndex].length) {
+                    roleElement.textContent += roles[roleIndex].charAt(letterIndex);
+                    letterIndex++;
+                } else {
+                    clearInterval(typingInterval);
+                    setTimeout(() => {
+                        roleIndex = (roleIndex + 1) % roles.length;
+                        typeRole();
+                    },1000);
+                }
+            }, 150);
+            roleElement.style.opacity = 1;
+        }, 500);
+    }
+    typeRole();
+});
