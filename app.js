7dcb4983ea215e61519021ba34d724e13e8cde2a
Chamith-Bhanuka <c.bhanukawp2002@gmail.com>
2025-05-03 23:18:30 +0530

    select html body to htmlElement variable

--- app.js
+++ app.js
@@ -52,6 +52,6 @@ document.addEventListener('DOMContentLoaded', function () {
 
 document.addEventListener('DOMContentLoaded', function () {
    const toggleButton = document.querySelector('.dark-light-btn');
-
+   const htmlElement = document.documentElement;
 });
 
