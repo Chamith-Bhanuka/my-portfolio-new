d8e886b0f16466ee0171098a640754f24e2c2c54
Chamith-Bhanuka <c.bhanukawp2002@gmail.com>
2025-05-04 00:40:52 +0530

    complete active link color change function

--- app.js
+++ app.js
@@ -82,10 +82,32 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 
-
-    //active section color change
-    const section = document.querySelectorAll('section');
+    const sections = document.querySelectorAll('section');
     const navLinks = document.querySelectorAll('ul li a');
+
+    function setActiveLink() {
+        let currentSection = '';
+
+        sections.forEach((section) => {
+            const sectionTop = section.offsetTop;
+            const sectionHeight = section.offsetHeight;
+
+            if (window.scrollY >= sectionTop - sectionHeight / 3) {
+                currentSection = section.getAttribute('id');
+            }
+        });
+
+        navLinks.forEach((link) => {
+            link.classList.remove('active');
+            if (link.getAttribute('href') === `#${currentSection}`) {
+                link.classList.add('active');
+            }
+        });
+    }
+
+    window.addEventListener('scroll', setActiveLink);
+
+
 });
 
 
